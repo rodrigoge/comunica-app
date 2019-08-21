20b0938f84cd21e75d21fb7b9d76a2c01394c5cb
rgestevao <rgestevao@gmail.com>
2019-08-21 16:36:21 -0300

    Utilização de DataTable na View

---
+++ Tcc/src/main/java/br/fepi/tcc/beans/ImagensBean.java
@@ -0,0 +1,34 @@
+package br.fepi.tcc.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.fepi.tcc.model.Imagem;
+
+@ManagedBean
+@SessionScoped
+public class ImagensBean {
+	
+	private List<Imagem> imagens = new ArrayList<>();
+	 
+	public List<Imagem> listarImagens()
+	{
+		imagens.add(new Imagem(0, "feliz.png", "Carinha Feliz"));
+		imagens.add(new Imagem(1, "triste.png","Carinha Triste"));
+		imagens.add(new Imagem(2, "bravo.png","Carinha Bravo"));
+		imagens.add(new Imagem(3, "brinquedos.png","Brincar"));
+		imagens.add(new Imagem(4, "escovar.png","Escovar os dentes"));
+		imagens.add(new Imagem(5, "estudar.png","Estudar"));
+		imagens.add(new Imagem(6, "comer.png","Comer"));
+	
+		return imagens;
+	}
+
+	public List<Imagem> getImagens() {
+		return imagens;
+	}
+
+}
---
+++ Tcc/src/main/java/br/fepi/tcc/model/Imagem.java
@@ -0,0 +1,60 @@
+package br.fepi.tcc.model;
+
+import java.io.Serializable;
+
+public class Imagem implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	private String descricao;
+	
+	public Imagem(int id, String nome, String descricao) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Imagem other = (Imagem) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+
+}
