4f229514235321159e4a89cec42afe1f3fbe8d45
rgestevao <rgestevao@gmail.com>
2019-09-23 15:47:36 -0300

    Atualizando interface da Prancha

--- Tcc/src/main/java/br/fepi/tcc/beans/ImagensBean.java
+++ Tcc/src/main/java/br/fepi/tcc/beans/ImagensBean.java
@@ -3,10 +3,12 @@ package br.fepi.tcc.beans;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
-import org.primefaces.event.DragDropEvent;
+import org.primefaces.event.ToggleEvent;
 
 import br.fepi.tcc.model.Imagem;
 
@@ -26,9 +28,23 @@ public class ImagensBean {
 		imagens.add(new Imagem(4, "escovar.png","Escovar os dentes"));
 		imagens.add(new Imagem(5, "estudar.png","Estudar"));
 		imagens.add(new Imagem(6, "comer.png","Comer"));
+		imagens.add(new Imagem(7, "feliz.png", "Carinha Feliz"));
+		imagens.add(new Imagem(8, "triste.png","Carinha Triste"));
+		imagens.add(new Imagem(9, "bravo.png","Carinha Bravo"));
+		imagens.add(new Imagem(10, "brinquedos.png","Brincar"));
+		imagens.add(new Imagem(11, "escovar.png","Escovar os dentes"));
+		imagens.add(new Imagem(12, "estudar.png","Estudar"));
+		imagens.add(new Imagem(13, "comer.png","Comer"));
 	
 		return imagens;
 	}
+	
+	 public void handleToggle(ToggleEvent event) {
+	        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Toggled", "Visibility:" + event.getVisibility());
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+	    }
+	 
+	 
 
 	public List<Imagem> getImagens() {
 		return imagens;
