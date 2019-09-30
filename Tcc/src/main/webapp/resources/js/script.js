1cc7783bfea63d231614f79187caaa369e22348c
rgestevao <rgestevao@gmail.com>
2019-09-29 21:20:40 -0300

    Otimizando interface de Rotina e criação de pontuação

--- Tcc/src/main/webapp/resources/js/script.js
+++ Tcc/src/main/webapp/resources/js/script.js
@@ -68,3 +68,117 @@ function incrementaValor3(valorMaximo) {
 	}
 	document.getElementById('pontos').value = value;
 }
+
+function incrementaValor4(valorMaximo) {
+	var value = parseInt(document.getElementById('pontos').value, 9);
+	var check = document.getElementById("check4");
+	if(check.checked)
+	{
+		value = isNaN(value) ? 0 : value;
+		if (value >= valorMaximo) {
+			value = valorMaximo;
+		} else {
+			value++;
+		}
+	}
+	else
+	{
+		value = value - 1;
+	}
+	document.getElementById('pontos').value = value;
+}
+
+function incrementaValor5(valorMaximo) {
+	var value = parseInt(document.getElementById('pontos').value, 9);
+	var check = document.getElementById("check5");
+	if(check.checked)
+	{
+		value = isNaN(value) ? 0 : value;
+		if (value >= valorMaximo) {
+			value = valorMaximo;
+		} else {
+			value++;
+		}
+	}
+	else
+	{
+		value = value - 1;
+	}
+	document.getElementById('pontos').value = value;
+}
+
+function incrementaValor6(valorMaximo) {
+	var value = parseInt(document.getElementById('pontos').value, 9);
+	var check = document.getElementById("check6");
+	if(check.checked)
+	{
+		value = isNaN(value) ? 0 : value;
+		if (value >= valorMaximo) {
+			value = valorMaximo;
+		} else {
+			value++;
+		}
+	}
+	else
+	{
+		value = value - 1;
+	}
+	document.getElementById('pontos').value = value;
+}
+
+function incrementaValor7(valorMaximo) {
+	var value = parseInt(document.getElementById('pontos').value, 9);
+	var check = document.getElementById("check7");
+	if(check.checked)
+	{
+		value = isNaN(value) ? 0 : value;
+		if (value >= valorMaximo) {
+			value = valorMaximo;
+		} else {
+			value++;
+		}
+	}
+	else
+	{
+		value = value - 1;
+	}
+	document.getElementById('pontos').value = value;
+}
+
+function incrementaValor8(valorMaximo) {
+	var value = parseInt(document.getElementById('pontos').value, 9);
+	var check = document.getElementById("check8");
+	if(check.checked)
+	{
+		value = isNaN(value) ? 0 : value;
+		if (value >= valorMaximo) {
+			value = valorMaximo;
+		} else {
+			value++;
+		}
+	}
+	else
+	{
+		value = value - 1;
+	}
+	document.getElementById('pontos').value = value;
+}
+
+function incrementaValor9(valorMaximo) {
+	var value = parseInt(document.getElementById('pontos').value, 9);
+	var check = document.getElementById("check9");
+	if(check.checked)
+	{
+		value = isNaN(value) ? 0 : value;
+		if (value >= valorMaximo) {
+			value = valorMaximo;
+		} else {
+			value++;
+		}
+	}
+	else
+	{
+		value = value - 1;
+	}
+	document.getElementById('pontos').value = value;
+}
